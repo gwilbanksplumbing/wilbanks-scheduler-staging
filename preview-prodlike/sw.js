394b8fb01407e8b1743824df12241761b6c04f79
Glenn Wilbanks <glenn@wilbanksplumbing.com>
2026-05-19 15:17:45 +0000

    preview-prodlike: deploy theme persistence (index-Eoa4kUqD.js + pre-hydration script), SW wc-v50

--- preview-prodlike/sw.js
+++ preview-prodlike/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wc-v49";
+const CACHE = "wc-v50";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
