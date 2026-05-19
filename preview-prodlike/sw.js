1314ed8575c761a7236e6e776ab67dee205f9ec7
Glenn Wilbanks <glenn@wilbanksplumbing.com>
2026-05-19 14:51:24 +0000

    preview-prodlike: deploy high-contrast light tokens (index-CNsW51l8.js / index-bymx2_sJ.css), SW wc-v46

--- preview-prodlike/sw.js
+++ preview-prodlike/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wc-v45";
+const CACHE = "wc-v46";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
