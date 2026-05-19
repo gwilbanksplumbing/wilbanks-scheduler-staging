af9bac1e80242c717b9c07c2f2391d1c43b03233
Glenn Wilbanks <glenn@wilbanksplumbing.com>
2026-05-19 14:22:10 +0000

    preview-prodlike: deploy white-bg light mode (index-D20ui3C7.js / index-Etj0ix8E.css), SW wc-v45

--- preview-prodlike/sw.js
+++ preview-prodlike/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wc-v44";
+const CACHE = "wc-v45";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
