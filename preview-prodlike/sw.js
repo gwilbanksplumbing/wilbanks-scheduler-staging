bb8b37c3c2566b836a02f9b9f4324161e0225dae
Glenn Wilbanks <gwillma@gmail.com>
2026-05-21 13:18:50 +0000

    preview-prodlike: customer row instant refresh after save (wc-v69 -> wc-v70)

--- preview-prodlike/sw.js
+++ preview-prodlike/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wc-v69";
+const CACHE = "wc-v70";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
