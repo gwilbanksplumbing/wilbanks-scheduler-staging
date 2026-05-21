85b83faa331b86c0061cf94d914f5288fc0cb625
Glenn Wilbanks <gwillma@gmail.com>
2026-05-21 14:29:27 +0000

    preview-prodlike: past days clickable in MonthView (wc-v73 -> wc-v74)
    Bundle: index-CCCrRgTc -> index-CIjeHnRB
    SW: wc-v73 -> wc-v74

--- preview-prodlike/sw.js
+++ preview-prodlike/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wc-v73";
+const CACHE = "wc-v74";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
