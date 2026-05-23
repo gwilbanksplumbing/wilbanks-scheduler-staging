ed401154fc50b9a8034db04210d7be142b81a8ef
Glenn Wilbanks <gwillba@gmail.com>
2026-05-23 02:23:28 +0000

    wc-v137 preview: overlay height = barTop - topOffset

--- preview-prodlike/sw.js
+++ preview-prodlike/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wc-v136";
+const CACHE = "wc-v137";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
