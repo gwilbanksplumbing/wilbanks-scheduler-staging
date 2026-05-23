8cf55fe6ca39b59036ec10c524b312917e12ddf8
Glenn Wilbanks <gwillma@gmail.com>
2026-05-23 02:29:11 +0000

    wc-v138: deploy 100dvh fix to preview-prodlike

--- preview-prodlike/sw.js
+++ preview-prodlike/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wc-v137";
+const CACHE = "wc-v138";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
