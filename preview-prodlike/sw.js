a44c86e84e9029547c7604f277b4d64ff73a0c12
Glenn Wilbanks <gwilbma@gmail.com>
2026-05-25 00:30:41 +0000

    fix(preview-prodlike): send ?checkNote=1 from dashboard preview calls + SW wc-v149

--- preview-prodlike/sw.js
+++ preview-prodlike/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wc-v148";
+const CACHE = "wc-v149";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
