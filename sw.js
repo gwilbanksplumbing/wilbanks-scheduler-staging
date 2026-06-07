a9210fec8c1f3b40cdd24eb1df30fa85e5a10141
Glenn Wilbanks <gwillma@gmail.com>
2026-06-07 00:12:53 +0000

    Adaptive calendar legend: Tech/Service/Status key matches active coloring + By Tech view; wc-v163

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// cache-bust: 20260606-2342 wc-v162 tech-color-swimlanes
-const CACHE = "wc-v162";
+// cache-bust: 20260606-2342 wc-v163 tech-color-swimlanes
+const CACHE = "wc-v163";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
