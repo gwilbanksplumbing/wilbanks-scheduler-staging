bca9bad6cc6a7a55c43484c3e629f0af8a51ff32
Glenn Wilbanks <gwillma@gmail.com>
2026-06-07 01:14:16 +0000

    New appt: honor clicked/seeded time; stop clamping to invalid current hour; keep seeded slot selectable (wc-v168)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// cache-bust: 20260606-2342 wc-v167 tech-color-swimlanes
-const CACHE = "wc-v167";
+// cache-bust: 20260606-2342 wc-v168 tech-color-swimlanes
+const CACHE = "wc-v168";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
