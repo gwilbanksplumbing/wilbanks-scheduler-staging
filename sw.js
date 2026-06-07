6afcb054dbe0710fa0b15698ac610911d75d68e3
Glenn Wilbanks <gwillma@gmail.com>
2026-06-07 01:51:00 +0000

    wc-v174: remove Color-by toggle (Status/Tech/Service) from calendar toolbar; color logic intact, defaults to status

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// cache-bust: 20260606-2342 wc-v173 tech-color-swimlanes
-const CACHE = "wc-v173";
+// cache-bust: 20260606-2342 wc-v174 tech-color-swimlanes
+const CACHE = "wc-v174";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
