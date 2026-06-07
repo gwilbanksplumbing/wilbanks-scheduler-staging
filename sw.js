d4cf1dcd3c9dd256cdf8b5d3182b926169d73af8
Glenn Wilbanks <gwillma@gmail.com>
2026-06-07 00:17:07 +0000

    By Tech view: drag a job along the time track to reschedule its time (snaps to nearest hour); cross-lane drop reassigns tech + time; wc-v164

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// cache-bust: 20260606-2342 wc-v163 tech-color-swimlanes
-const CACHE = "wc-v163";
+// cache-bust: 20260606-2342 wc-v164 tech-color-swimlanes
+const CACHE = "wc-v164";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
