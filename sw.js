8e2de5c94d412887dd43be22401e183bb7ae1469
Glenn Wilbanks <gwillma@gmail.com>
2026-06-07 01:25:33 +0000

    By Tech view: block scheduling on ALL past days (not just today); full-day shade + no pointer (wc-v171)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// cache-bust: 20260606-2342 wc-v170 tech-color-swimlanes
-const CACHE = "wc-v170";
+// cache-bust: 20260606-2342 wc-v171 tech-color-swimlanes
+const CACHE = "wc-v171";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
