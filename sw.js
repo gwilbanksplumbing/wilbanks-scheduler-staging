a10134e76c3045c53021efeba07cfc581303c928
Glenn Wilbanks <gwillma@gmail.com>
2026-06-07 01:07:15 +0000

    By Tech view: click open slot to schedule new appt (prefills tech + hour) (wc-v166)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// cache-bust: 20260606-2342 wc-v165 tech-color-swimlanes
-const CACHE = "wc-v165";
+// cache-bust: 20260606-2342 wc-v166 tech-color-swimlanes
+const CACHE = "wc-v166";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
