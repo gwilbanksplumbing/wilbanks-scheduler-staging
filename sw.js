f33317c00d53eaf1c0518d8a3d8c92047a3ca0ca
Glenn Wilbanks <gwillma@gmail.com>
2026-06-08 01:15:47 +0000

    wc-v197: deploy non-scrolling sidebar + Theme-in-Settings (JS index-Dtl0JF_c, CSS index-DBqBvYr6)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// cache-bust: 20260606-2342 wc-v196 tech-color-swimlanes
-const CACHE = "wc-v196";
+// cache-bust: 20260606-2342 wc-v197 tech-color-swimlanes
+const CACHE = "wc-v197";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
