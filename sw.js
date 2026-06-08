e4c34c28efc4a46c4f29c44728dd9fad4f32ecb2
Glenn Wilbanks <gwillma@gmail.com>
2026-06-08 02:11:17 +0000

    wc-v201: Audit Log + Deleted Jobs Settings tabs (deploy)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// cache-bust: 20260606-2342 wc-v200 tech-color-swimlanes
-const CACHE = "wc-v200";
+// cache-bust: 20260606-2342 wc-v201 tech-color-swimlanes
+const CACHE = "wc-v201";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
