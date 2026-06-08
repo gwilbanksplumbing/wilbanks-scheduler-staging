eb9f2fa91697bc35745bd5867597bf7e6fe9711d
Glenn Wilbanks <gwillma@gmail.com>
2026-06-08 02:54:18 +0000

    wc-v206: deploy Admin Tools hover-flyout picker (JS+CSS swap, sw bump)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// cache-bust: 20260606-2342 wc-v205 tech-color-swimlanes
-const CACHE = "wc-v205";
+// cache-bust: 20260606-2342 wc-v206 tech-color-swimlanes
+const CACHE = "wc-v206";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
