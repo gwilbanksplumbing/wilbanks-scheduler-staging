beda22c8e49ec4b69578435e1c4dc9c4c155eb59
Glenn Wilbanks <gwillma@gmail.com>
2026-06-08 13:13:32 +0000

    v215: deploy restructured tab row (index-65oZMqeY.js / index-F-9TF_dQ.css, sw wc-v215)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// cache-bust: 20260606-2342 wc-v214 tech-color-swimlanes
-const CACHE = "wc-v214";
+// cache-bust: 20260606-2342 wc-v215 tech-color-swimlanes
+const CACHE = "wc-v215";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
