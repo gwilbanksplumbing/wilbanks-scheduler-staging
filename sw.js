ce568f16c9db1c2a1e3d293217a285a1b0d514a6
Glenn Wilbanks <gwillma@gmail.com>
2026-06-08 11:30:53 +0000

    wc-v210: deploy mobile Settings link (JS swap, sw bump, LOADED_JS sync)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// cache-bust: 20260606-2342 wc-v209 tech-color-swimlanes
-const CACHE = "wc-v209";
+// cache-bust: 20260606-2342 wc-v210 tech-color-swimlanes
+const CACHE = "wc-v210";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
