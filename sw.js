9568ad26e8121998a9e454aba59f7c827fa59e05
Glenn Wilbanks <gwillma@gmail.com>
2026-06-08 02:58:20 +0000

    wc-v207: deploy hover-only pill highlight (JS swap, sw bump)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// cache-bust: 20260606-2342 wc-v206 tech-color-swimlanes
-const CACHE = "wc-v206";
+// cache-bust: 20260606-2342 wc-v207 tech-color-swimlanes
+const CACHE = "wc-v207";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
