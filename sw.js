4fa491a7e259a145fa9632ed12b335ba39163b6f
Glenn Wilbanks <gwillma@gmail.com>
2026-06-08 01:44:33 +0000

    wc-v198: deploy Techs->Settings move + hover-pill flyout (JS index-ClMVSzS4, CSS index-DzqAvw0T)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// cache-bust: 20260606-2342 wc-v197 tech-color-swimlanes
-const CACHE = "wc-v197";
+// cache-bust: 20260606-2342 wc-v198 tech-color-swimlanes
+const CACHE = "wc-v198";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
