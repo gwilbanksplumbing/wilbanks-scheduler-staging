4ee7a5584c0b257c7a7f169821efaffff625b2a4
Glenn Wilbanks <gwillma@gmail.com>
2026-06-08 13:07:20 +0000

    v213: deploy tab row scroll bleed-through fix (index-CK0ccQB9.js / index-BjzxFSys.css, sw wc-v213)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// cache-bust: 20260606-2342 wc-v212 tech-color-swimlanes
-const CACHE = "wc-v212";
+// cache-bust: 20260606-2342 wc-v213 tech-color-swimlanes
+const CACHE = "wc-v213";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
