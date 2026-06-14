594962a208a77cdf74539bd46f8b991275de7896
Glenn Wilbanks <gwillma@gmail.com>
2026-06-14 14:26:56 +0000

    Revert Day-view '+' add affordance; keep fmtTime NaN em-dash fix; bundle index-6EALcl5J; SW wc-v295b

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "wc-v295a";
+const CACHE = "wc-v295b";
 const OFFLINE = ["/", "/index.html"];
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE)));
